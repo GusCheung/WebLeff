b00791fe352377ee61fb2e7487e47ea7acfa63f7
GusCheung <1554782483@qq.com>
2020-05-11 19:44:19 +0800

    更新代码

--- web_login/models.py
+++ web_login/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+class Xtyh(models.Model):
+    用户编号 = models.CharField(primary_key=True, max_length=20)
+    用户姓名 = models.CharField(max_length=30, blank=True, null=True)
+    部门 = models.CharField(max_length=20, blank=True, null=True)
+    密码 = models.CharField(max_length=32, blank=True, null=True)
+    email = models.CharField(max_length=200)
+    背景图片 = models.CharField(max_length=50)
+    公司名称 = models.CharField(max_length=50)
+    钉钉id = models.CharField(db_column='钉钉ID', max_length=50)  # Field name made lowercase.
+    备注 = models.CharField(max_length=50)
+    制单人 = models.CharField(max_length=20)
+    制单人_name = models.CharField(max_length=30, blank=True, null=True)
+    制单日期 = models.DateTimeField()
+    更新人 = models.CharField(max_length=20)
+    更新人_name = models.CharField(max_length=30, blank=True, null=True)
+    更新日期 = models.DateTimeField()
+    失效标志 = models.BooleanField()
+    是否可以查看所有客户资料 = models.BooleanField()
+
+    class Meta:
+        managed = True
+        db_table = '系统用户'
+
+
+# 操作记录表
+class WebLogin(models.Model):
+    date = models.DateTimeField(blank=True, null=True)
+    appname = models.CharField(max_length=50, blank=True, null=True)
+    apppage = models.CharField(max_length=50, blank=True, null=True)
+    host = models.CharField(db_column='Host', max_length=50, blank=True, null=True)  # Field name made lowercase.
+    username = models.CharField(max_length=50, blank=True, null=True)
+    date2 = models.DateField(blank=True, null=True)
+    class Meta:
+        managed = True
+        db_table = 'django_web_login记录'
--- web_login/views.py
+++ web_login/views.py
@@ -1,9 +1,36 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse, JsonResponse
 from django.shortcuts import render, redirect, reverse
-
+import hashlib #MD5解密
+from web_login.models import Xtyh, WebLogin
+import datetime
 # Create your views here.
 
 
 def login(request):
-    return render(request, 'login.html')
+    if request.method == "get":
+        return render(request, 'login.html')
+    if request.method == "post":
+        login_info = request.POST
+        username = login_info.get('user', None)
+        userpwd = login_info.get('pwd', None)
+        userpwd = hashlib.md5(userpwd.encode("utf8")).hexdigest()
+        userpwd = userpwd.upper()
+        if username != "":
+            if userpwd != "":
+                try:
+                   db_系统用户 = Xtyh.objects.get(用户编号=username)
+                   if db_系统用户.密码 != userpwd:
+                       return render(request, 'login.html', {'msg': "密码错误"})
+                   else:
+                       now_today = datetime.datetime.now()
+                       now_today_day = datetime.date.today()
+                       request.session['is_login'] = True
+                       request.session['user_name'] = username
+                       request.session.set_expiry(0)
+                       weblogin = WebLogin(username=username, apppage='首页', appname='web_xx控制台', date=now_today,
+                                           date2=now_today_day)
+                       weblogin.save()
+                       return HttpResponse("登录成功")
+                except:
+                    return render(request, "login.html", {'msg': "无此账号"})
