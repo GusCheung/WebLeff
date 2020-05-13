971ec2ec7df151f3f16b5b62d5149ec71561b531
GusCheung <1554782483@qq.com>
2020-05-13 20:00:59 +0800

    更新文件夹

--- web_login/urls.py
+++ web_login/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from web_login.views import login,index,user_logout,ding_login
-
+from web_login.views import login, index, user_logout, ding_login
+from django.conf.urls import url
 urlpatterns = [
     path('', login, name='login'),
     path('index', index, name='index'),
     path('user_logout', user_logout, name='user_logout'),
-    path('ding_login', ding_login, name='ding_login')
+    url(r'^ding_login/?', ding_login)
 ]
